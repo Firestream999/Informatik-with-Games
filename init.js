d8ce3a30f8c19c0e2a2ab3ca0f7e715076b09d06
Firestream999 <robin@alt-morbach.de>
2022-07-01 16:40:59 +0200

    v4.1.1
    v4.1.1

--- init.js
+++ init.js
@@ -1,11 +1,11 @@
 
     (function () {
         var options = {
-            link: "https://www.chatroom2000.de/", // Link
-            link_color: "#E74339", // Link button color
-            call_to_action: "Schreibt euch", // Call to action
-            button_color: "#FF6550", // Color of button
-            position: "right", // Position may be 'right' or 'left'
+            link: "https://www.chatroom2000.de/", 
+            link_color: "#E74339", 
+            call_to_action: "Schreibt euch", 
+            button_color: "#FF6550", 
+            position: "right", 
         };
         var proto = 'https:', host = "getbutton.io", url = proto + '//static.' + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
